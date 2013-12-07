ceb566a265880b85b2bb5365024b0203f437d1ce
Lioznov Anton <Lavton@gmail.com>
2013-12-07 15:54:33 +0400

    delete buil.xml

--- src/MySocket.java
+++ src/MySocket.java
@@ -1,10 +1,25 @@
-/**
- * Created with IntelliJ IDEA.
- * User: lavton
- * Date: 07.12.13
- * Time: 12:01
- * To change this template use File | Settings | File Templates.
- */
+import java.net.ServerSocket.*;
+import java.net.Socket.*;
+import java.net.*;
+import java.util.*;
+import java.io.*;
 public class MySocket {
+    public static final int port = 692_324_999;
+    public void workAsServer(){
+        try (ServerSocket serverSocket = new ServerSocket(port)) {
+            try (Socket socket = serverSocket.accept()) {
+                InputStream inputStream = socket.getInputStream();
+                while (true){
+                    int whoDidThis;
+                    int whatHeDid;
+                    whoDidThis = inputStream.read();
+                    whatHeDid = inputStream.read();
+                }
 
+            }
+
+        }catch (Exception e){
+            e.printStackTrace();
+        }
+    }
 }
