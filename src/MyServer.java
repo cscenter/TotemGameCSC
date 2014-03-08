cb37e998f1d6438a5b69bf3d57bd295e8c2485f9
Lioznov Anton <Lavton@gmail.com>
2014-03-08 11:51:15 +0400

    fixed initialization on server

--- src/MyServer.java
+++ src/MyServer.java
@@ -6,6 +6,8 @@ import java.io.*;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
+import static java.lang.Thread.sleep;
+
 public class MyServer {
     public int whoDidThis;
     public int whatHeDid;
@@ -17,14 +19,21 @@ public class MyServer {
     public static final int port = 6923;
 
     public MyServer(){
+        clientOutput = new LinkedList<>();
+        clients = new LinkedList<>();
+        clientInput = new LinkedList<>();
+        comands = new ArrayDeque<>(100);
+
         try (ServerSocket serverSocket = new ServerSocket(port)) {
             //подключили всех пользователей
             for (int i=0; i<numberOfPl; i++){
-                try (Socket socket = serverSocket.accept()) {
+                try {
+                    Socket socket = serverSocket.accept();
                     clients.add(socket);
                     InputStream inputStream = socket.getInputStream();
                     clientInput.add(inputStream);
                     clientOutput.add(socket.getOutputStream());
+
                 }catch (Exception e){
                     throw e;
                 }
@@ -33,12 +42,17 @@ public class MyServer {
             //генерим кто первый ходит и зерно для случайного набора карт
             byte firstPlayer = (byte)(((new Random()).nextInt())%numberOfPl);
             byte cardSeed = (byte)((new Random()).nextInt());
-
+            System.err.println("HEre");
             //пересылаем это всем
             for (OutputStream os : clientOutput){
                 os.write(firstPlayer);
                 os.write(cardSeed);
                 os.flush();
+            }
+            System.err.println("HEre");
+
+            for (Socket socket : clients){
+                socket.close();
             }
         }catch (Exception e){
             e.printStackTrace();
